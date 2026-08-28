4ef2950a9b35f032d69838ade3657594093d62bb
Jouni Juntunen <jouni.juntunen@oamk.fi>
2026-08-28 10:18:02 +0300

    Fixed lint errors.

--- api/middleware/errorHandler.js
+++ api/middleware/errorHandler.js
@@ -1,9 +1,9 @@
-const errorHandler = (err, req, res, next) => {
+const errorHandler = (err, req, res, _next) => {
   const statusCode = err.status || 500
   res.status(statusCode).json({
     error: {
       message: err.message,
-      status: statusCode  
+      status: statusCode
     }
   })
 }
